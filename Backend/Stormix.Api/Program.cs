1e9727a1b0a537ccfc051d8d79b490b38e504ded
Amro Aladghem <ameraladghem@gmail.com>
2026-06-12 22:40:49 +0300

    Creating AddRequestVideo & VideoExtracted Repo , Creating Controller phase 1

--- Backend/Stormix.Api/Program.cs
+++ Backend/Stormix.Api/Program.cs
@@ -1,4 +1,9 @@
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Data;
 using Infrastructure.ExternalServices;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
 using Mscc.GenerativeAI;
 using Mscc.GenerativeAI.Types;
 
@@ -14,11 +19,19 @@ builder.Services.AddHttpClient("QdrantClient", (serviceProvider, client) =>
     client.DefaultRequestHeaders.Add("api-key", configuration["Qdrant:ApiKey"]);
 });
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration["SQL_CONNECTION_STRING"]);
+});
+
 builder.Services.AddSingleton<GoogleAI>(sp => new GoogleAI(apiKey: builder.Configuration.GetSection("geminiApi").Value!));
 builder.Services.AddSingleton<GenerationConfig>(gc => new GenerationConfig() { ResponseMimeType = "application/json", Temperature = 0 });
 builder.Services.AddScoped<AiEmbeddingService>();
 builder.Services.AddScoped<QdrantSegmentSearchService>();
 builder.Services.AddScoped<SearchResultService>();
+builder.Services.AddScoped<IManagerRepository, ManagerRepository>();
+builder.Services.AddScoped<AddRequestVideoService>();
+builder.Services.AddScoped<VideoExtractedService>();
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(Presentation.AssemblyRefference).Assembly);
